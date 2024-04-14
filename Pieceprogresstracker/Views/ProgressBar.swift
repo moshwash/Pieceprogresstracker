a638583e995c3460ddd6442288b4cdc2808ba07e
Oleg D <moshwash@yandex.ru>
2024-04-14 21:23:28 +0300

    Test upd w progress bar

--- Pieceprogresstracker/Views/ProgressBar.swift
+++ Pieceprogresstracker/Views/ProgressBar.swift
@@ -2,57 +2,17 @@
 //  ProgressBar.swift
 //  Pieceprogresstracker
 //
-//  Created by Oleg D on 13.03.2024.
+//  Created by Oleg D on 14.04.2024.
 //
 
 import SwiftUI
 
 struct ProgressBar: View {
-    var piece: Piece
-    
     var body: some View {
-        VStack {
-            HStack {
-                Text("Bars learned: 15 / \(piece.bars)")
-                Spacer()
-                Text("70%")
-                    .foregroundColor(Color.blue)
-                
-            }
-            .font(.caption)
-            
-            // затестить код из чатЖПТ, попробовать связать с кнопками
-            
-            HStack {
-                Button(action: {
-                    
-                }, label: {
-                    Text("- Bar")
-                        .foregroundStyle(Color(.black))
-                        .frame(maxWidth: .infinity)
-                })
-                .padding(6)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 100.0)
-                        .stroke(.black, lineWidth: 1)
-                )
-                
-                Spacer()
-                
-                Button(action: {
-                    
-                }, label: {
-                    Text("+ Bar")
-                        .frame(maxWidth: .infinity)
-                })
-                .clipShape(RoundedRectangle(cornerRadius: 100.0))
-                .buttonStyle(.borderedProminent)
-                
-            }
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ProgressBar(piece: pieces[0])
+    ProgressBar()
 }
